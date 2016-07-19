6c3330bef935482ab0f17e6ff6ca695083befb94
WingedAshur6 <ddfe530tx@gmail.com>
2016-07-19 16:37:11 -0500

    Next iteration of code
    this part of the code gathers all barrels then takes their states.

--- test_StateAnalyzer_new.py
+++ test_StateAnalyzer_new.py
@@ -16,7 +16,8 @@ def is_number(s):
 
 def make_time(unix_time):
     return matplotlib.dates.date2num()
-
+def COLUMN(matrix, i):
+    return [row[i] for row in matrix]
         
 class StateAnalyzer:
     def __init__(self, log_file):
@@ -29,8 +30,30 @@ class StateAnalyzer:
                 # self.data[-1].append(int(i))
         
         self.data = np.fromfile('773logtest2_TEST.log', int, -1, "\t")
-        self.columns=39 # I counted the actual columns in the output data
-        self.data=np.reshape(self.data,(np.size(self.data)/self.columns,self.columns)) # to reshape the newly aqcuired dataset to represent the output data
+        self.columns=39#------------------------------------------------------------------------------------------------- I actually went and counted the number of columns in the output data
+        self.data=np.reshape(self.data,(np.size(self.data)/self.columns,self.columns))#---------------------------------- to reshape the newly aqcuired dataset to represent the output data
+        
+        
+    def StateIdentifier(self):
+        self.number_of_data=7 #------------------------------------------------------------------------------------------ this is the number of columns of data per barrel.
+        self.number_of_barrels=(np.size(self.data,1)-11)/self.number_of_data
+        IPD_Columns=np.array([None,None,None,None],dtype=object)
+        DEF_Columns=np.array([None,None,None,None],dtype=object)
+        IPD_Columns=IPD_Columns[0:self.number_of_barrels]
+        DEF_Columns=DEF_Columns[0:self.number_of_barrels]
+        
+        for barrel in range(self.number_of_barrels):
+            print barrel
+            #print COLUMN(self.data,10+5+7*barrel)
+
+            IPD_Columns[barrel]=np.append(IPD_Columns[barrel],COLUMN(self.data,10+5+7*barrel))
+            DEF_Columns[barrel]=np.append(DEF_Columns[barrel],COLUMN(self.data,10+6+7*barrel))
+       
+        IPD_Columns=np.transpose(IPD_Columns)
+        DEF_Columns=np.transpose(DEF_Columns)
+        state_columns=([IPD_Columns, DEF_Columns])#---------------------------------------------------------------------- this gets all the number of barrels and puts them together.
+        return state_columns#-------------------------------------------------------------------------------------------- this will return as: [State:1->2][Barrel:1->4][time:START->END]
+    
         
         
        
@@ -39,7 +62,10 @@ s=StateAnalyzer('773logtest2_TEST.log')
         
 print s.data
 print np.shape(s.data)        
-        
+Data=s.StateIdentifier()
+print Data
+print np.shape(Data[0][1])
+print Data[0][1][1:2000]
         
         
         
@@ -53,7 +79,7 @@ print np.shape(s.data)
 		
 		# s = State.initialize()
 		# s.barrel_number = 3
-		# s.state_name    = "DEF"
+		# s.state_name    = "IPD"
 		# ...
 
 		# for each state detected:
